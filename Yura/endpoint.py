5f59749def30a67b8784e5a173c603fe94f3fc62
Yuriy Antentyk <antentyk@ucu.edu.ua>
2017-12-03 12:01:31 +0200

    done endpoint.py

--- Yura/endpoint.py
+++ Yura/endpoint.py
@@ -3,4 +3,11 @@ class Endpoint:
         # cacheslst - list of id of caches
         # requests - (video id, number of requests)
         # datacenterdelay - int
-        pass
+        self.caches = set()
+        for item in cacheslst:
+            self.caches.add(item)
+        self.requests = {}
+        for item in requests:
+            videoid, number = item[0], item[1]
+            self.requests[videoid] = number
+        self.datacenterdelay = detacenterdelay
