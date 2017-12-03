69654f4f33d980791d54583ff8d2310cc6f0db25
Yuriy Antentyk <antentyk@ucu.edu.ua>
2017-12-03 11:54:12 +0200

    added structure

---
+++ Yura/endpoint.py
@@ -0,0 +1,6 @@
+class Endpoint:
+    def __init__(self, cacheslst, requests, datacenterdelay):
+        # cacheslst - list of id of caches
+        # requests - (video id, number of requests)
+        # datacenterdelay - int
+        pass
