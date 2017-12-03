232eb6ab237779af1aed304d6538105c36b6e78e
Volodymyr Zabulskyy <v.zabulskyy@gmail.com>
2017-12-03 13:23:21 +0200

    __str__ for Caches

--- Yura/cache.py
+++ Yura/cache.py
@@ -1,6 +1,17 @@
 class Cache:
-    def __init__(self ,capacity):
+    def __init__(self, capacity):
+        self.id = -1
         self.capacity = capacity
-        self.endpointsids = [] # id
+        self.endpointsids = []  # id
         self.taken = set()
         self.needs = [{} for _ in range(10000)]
+
+    def __str__(self):
+        return """
+id: {0}
+capacity: {1}
+endpointsids: {2}
+taken: {3}
+needs: {4}
+
+""".format(self.id, self.capacity, self.endpointsids, self.taken, self.needs)
--- Yura/read.py
+++ Yura/read.py
@@ -1,29 +1,38 @@
 from Yura.endpoint import Endpoint
 from Yura.cache import Cache
 
-V, E, R, C, X = map(int, input().split())
 
-videos = list(map(int, input().split()))
-endpoints = [Endpoint() for _ in range(E)]
-caches = [Cache(X) for _ in range(C)]
+def read_input():
+    V, E, R, C, X = map(int, input().split())
 
-for i in range(E):
-    l, k = map(int, input().split())
-    endpoints[i].datacenterdistance = l
-    for j in range(k):
-        cacheid, l = map(int, input().split())
-        endpoints[i].cachesdistance.append((cacheid, l))
-        caches[cacheid].endpointsids.append(i)
+    videos = list(map(int, input().split()))
+    endpoints = [Endpoint() for _ in range(E)]
+    caches = [Cache(X) for _ in range(C)]
 
-for i in range(R):
-    videoid, endpointid, number = map(int, input().split())
-    endpoints[endpointid].id = endpointid
-    endpoints[endpointid].requests.append((videoid, number))
-for i in endpoints:
-    print(i)
+    for i in range(E):
+        l, k = map(int, input().split())
+        endpoints[i].datacenterdistance = l
+        for j in range(k):
+            cacheid, l = map(int, input().split())
+            endpoints[i].cachesdistance.append((cacheid, l))
+            caches[cacheid].endpointsids.append(i)
+            caches[cacheid].id = cacheid
+
+    for i in range(R):
+        videoid, endpointid, number = map(int, input().split())
+        endpoints[endpointid].id = endpointid
+        endpoints[endpointid].requests.append((videoid, number))
+    # for i in endpoints:
+    #     print(i)
+    # for i in caches:
+    #     print(i)
+    return endpoints, caches
+
+
 """
 for e in endpoints:
     for c in e.cachesdistance:
         cacheid, distance = c[0], c[1]
         caches[cacheid].
 """
+read_input()
